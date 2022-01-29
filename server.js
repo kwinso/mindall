328c7bd7644ce324e1463fff8bc46f3bf4f6acb8
uwumouse <pythonisajoke@gmail.com>
2022-01-29 22:19:47 +0300

    I tought that's for the old client, lol.

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.set('view engine', 'pug');
 app.set('view options', { basedir: __dirname });
 app.locals.basedir = path.join(__dirname, './views');
 // Using new version for app
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(cors({ origin: "*" }));
 //#endregion
 
